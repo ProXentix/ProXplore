d570ef89a468359dd13ae87e10bcdbc9e564a5e5
ProgrammerKR <programmerkr.123@gmail.com>
2026-02-17 13:41:45 +0530

    Fix build errors: Unignore firebaseConfig and update expo-updates

---
+++ firebaseConfig.js
@@ -0,0 +1,6 @@
+import auth from '@react-native-firebase/auth';
+
+// No initialization needed here as @react-native-firebase/app 
+// handles it automatically using google-services.json
+
+export { auth };
